d172a6b97d14c605d28f059fd0c2c19a8ee6f02d
Teemu Kulma <teemu.kulma@iki.fi>
2020-10-27 19:22:38 +0200

    chapter 7-72

--- src/App.svelte
+++ src/App.svelte
@@ -22,6 +22,7 @@
         'https://upload.wikimedia.org/wikipedia/commons/6/65/Baby.tux-800x800.png',
       address: '27th Nerd Road, 32423 New York',
       contactEmail: 'code@test.com',
+      isFavorite: false,
     },
     {
       id: 'm2',
@@ -32,6 +33,7 @@
         'https://images.squarespace-cdn.com/content/v1/551b7247e4b04e2cba1e41ce/1527818395733-WHRPPNMWOHVJ7QLBH8RZ/ke17ZwdGBToddI8pDm48kM27ZZI47ic5XRBZeHEXLbx7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0pTKqSDRwmMK43IUI3HojJVzs4UH6zJyplqGEef6zS_HQ5jKL-0x7Oc_ciLAaGkE5A/babydrop.jpg?format=2500w',
       address: '27th Nerd Road, 32423 New York',
       contactEmail: 'code@test.com',
+      isFavorite: false,
     },
   ];
 
@@ -47,6 +49,16 @@
     };
 
     meetups = [newMeetup, ...meetups];
+  }
+
+  function toggleFavorite(event) {
+    const id = event.detail;
+    const updatedMeetup = { ...meetups.find((m) => m.id === id) };
+    updatedMeetup.isFavorite = !updatedMeetup.isFavorite;
+    const meetupIndex = meetups.findIndex((m) => m.id === id);
+    const updatedMeetups = [...meetups];
+    updatedMeetups[meetupIndex] = updatedMeetup;
+    meetups = updatedMeetups;
   }
 </script>
 
@@ -106,5 +118,5 @@
 
     <Button type="submit" caption="Save" />
   </form>
-  <MeetupGrid {meetups} />
+  <MeetupGrid {meetups} on:togglefavorite={toggleFavorite} />
 </main>
