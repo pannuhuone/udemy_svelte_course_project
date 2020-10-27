04e04fd4d041e99e843844f799a93fb3b457af1c
Teemu Kulma <teemu.kulma@iki.fi>
2020-10-27 19:30:57 +0200

    chapter 7-74

--- src/Meetups/MeetupItem.svelte
+++ src/Meetups/MeetupItem.svelte
@@ -1,6 +1,7 @@
 <script>
   import { createEventDispatcher } from 'svelte';
   import Button from '../UI/Button.svelte';
+  import Badge from '../UI/Badge.svelte';
 
   export let id;
   export let title;
@@ -74,7 +75,12 @@
 
 <article>
   <header>
-    <h1>{title}</h1>
+    <h1>
+      {title}
+      {#if isFav}
+        <Badge>FAVORITE</Badge>
+      {/if}
+    </h1>
     <h2>{subtitle}</h2>
     <p>{address}</p>
   </header>
@@ -86,6 +92,7 @@
     <Button href="mailto:{email}" caption="Contact" />
     <Button
       mode="outline"
+      color={isFav ? null : 'success'}
       type="button"
       caption={isFav ? 'Unfavorite' : 'Favorite'}
       on:click={() => dispatch('togglefavorite', id)} />
