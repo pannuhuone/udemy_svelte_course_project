41745949ce0ce4d5cd8a132554a8fe67fbdd9ee7
Teemu Kulma <teemu.kulma@iki.fi>
2020-10-27 19:58:35 +0200

    chapter 7-74

--- src/App.svelte
+++ src/App.svelte
@@ -3,13 +3,9 @@
   import MeetupGrid from './Meetups/MeetupGrid.svelte';
   import TextInput from './UI/TextInput.svelte';
   import Button from './UI/Button.svelte';
+  import EditMeetup from './Meetups/EditMeetup.svelte';
 
-  let title = '';
-  let subtitle = '';
-  let address = '';
-  let email = '';
-  let description = '';
-  let imageUrl = '';
+  let editMode;
 
   let meetups = [
     {
@@ -66,57 +62,14 @@
   main {
     margin-top: 5rem;
   }
-
-  form {
-    width: 30rem;
-    max-width: 90%;
-    margin: auto;
-  }
 </style>
 
 <Header />
 
 <main>
-  <form on:submit|preventDefault={addMeetup}>
-    <TextInput
-      id="title"
-      label="Title"
-      value={title}
-      on:input={(event) => (title = event.target.value)} />
-
-    <TextInput
-      id="subtitle"
-      label="Subtitle"
-      value={subtitle}
-      on:input={(event) => (subtitle = event.target.value)} />
-
-    <TextInput
-      id="address"
-      label="Address"
-      value={address}
-      on:input={(event) => (address = event.target.value)} />
-
-    <TextInput
-      id="imageUrl"
-      label="ImageUrl"
-      value={imageUrl}
-      on:input={(event) => (imageUrl = event.target.value)} />
-
-    <TextInput
-      id="email"
-      label="Email"
-      type="email"
-      value={email}
-      on:input={(event) => (email = event.target.value)} />
-
-    <TextInput
-      id="description"
-      label="Description"
-      controlType="textarea"
-      value={description}
-      on:input={(event) => (description = event.target.value)} />
-
-    <Button type="submit" caption="Save" />
-  </form>
+  <Button caption="New Meetup" on:click={() => (editMode = 'add')} />
+  {#if editMode === 'add'}
+    <EditMeetup />
+  {/if}
   <MeetupGrid {meetups} on:togglefavorite={toggleFavorite} />
 </main>
